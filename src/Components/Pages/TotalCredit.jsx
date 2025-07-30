e92292d81a98376dd323257f5b6025fa8b37bb81
rosheesh7 <rosissharma87@gmail.com>
2025-07-30 21:36:45 +0545

    Adding Total Credit

--- src/Components/Pages/TotalCredit.jsx
+++ src/Components/Pages/TotalCredit.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const TotalCredit = ({ totalCredit, transactionCount}) => {
-   
+const TotalCredit = ({ totalCredit, transactionCount }) => {
   return (
     <>
-
-    <div className='Total-Expense-Container'>
-        <div className='Total-Expense-Title'>
-            <span  className='Total-Expense-Title-span1'>Credit Available</span>
-            <span className='Total-Expense-Title-span2'>$</span>
+      <div className="Total-Expense-Container">
+        <div className="Total-Expense-Title">
+          <span className="Total-Expense-Title-span1">Credit Available</span>
+          <span className="Total-Expense-Title-span2">$</span>
         </div>
 
-        <div className='Total-Expense-Value'>
-            <span style={{ color: totalCredit>=0 ? "gold" : "red" }}>${totalCredit}</span>
+        <div className="Total-Expense-Value">
+          <span style={{ color: totalCredit >= 0 ? "gold" : "red" }}>
+            ${totalCredit.toFixed(2)}
+          </span>
         </div>
 
-        <div className='Total-Expense-Transactions'>
-          <span> {transactionCount} transations recorded </span>  
+        <div className="Total-Expense-Transactions">
+          <span> {transactionCount} transations recorded </span>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default TotalCredit
+export default TotalCredit;
--- src/Components/Pages/TotalExpense.jsx
+++ src/Components/Pages/TotalExpense.jsx
@@ -1,25 +1,24 @@
+import React, { use, useState } from "react";
 
-import React, { use, useState } from 'react'
-
-const TotalExpense = ({ totalExpenses , transactionCount }) => {
+const TotalExpense = ({ totalExpenses, transactionCount }) => {
   return (
     <>
-    <div className='Total-Expense-Container'>
-        <div className='Total-Expense-Title'>
-            <span  className='Total-Expense-Title-span1'>Total Expense</span>
-            <span className='Total-Expense-Title-span2'>$</span>
+      <div className="Total-Expense-Container">
+        <div className="Total-Expense-Title">
+          <span className="Total-Expense-Title-span1">Total Expense</span>
+          <span className="Total-Expense-Title-span2">$</span>
         </div>
 
-        <div className='Total-Expense-Value'>
-            <span style={{color: "gold"}}>${totalExpenses}</span>
+        <div className="Total-Expense-Value">
+          <span style={{ color: "gold" }}>${totalExpenses.toFixed(2)}</span>
         </div>
 
-        <div className='Total-Expense-Transactions'>
-          <span> {transactionCount} transations recorded </span>  
+        <div className="Total-Expense-Transactions">
+          <span> {transactionCount} transations recorded </span>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default TotalExpense
+export default TotalExpense;
